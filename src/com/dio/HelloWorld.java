d5165b4191a3026ba99a9ab23500a1519e0a8c58
arthurvenicio <stuks.midia@gmail.com>
2021-10-16 02:46:18 -0300

    first commit

---
+++ src/com/dio/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.dio;
+
+import com.dio.models.Cat;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Cat cat1 = new Cat();
+		System.out.println(cat1);
+	}
+
+}
---
+++ src/com/dio/models/Cat.java
@@ -0,0 +1,45 @@
+package com.dio.models;
+
+public class Cat {
+	
+	private String name;
+	private String color;
+	private Integer age;
+	
+	public Cat() {
+	}
+
+	public Cat(String name, String color, Integer age) {
+		this.name = name;
+		this.color = color;
+		this.age = age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+	
+}
